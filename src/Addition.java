96e5d22664b099c51d9018cbc9cea0bc1cbcac01
josepht273 <josephtadessem@gmail.com>
2025-03-23 11:48:45 +0300

    Bitwise Oprators in java

--- src/Addition.java
+++ src/Addition.java
@@ -21,6 +21,18 @@ public class Addition {
         System.out.println("Sub " + (x - y));
         System.out.println("Multi " + x * y);
         System.out.println("Div " + x / y);
+
+        byte c = 64, d;
+        int i = c << 2; // Multiply the value 2 times the shift value
+        int j = c >> 2; // Divided the value by two and discard the reminder
+        d = (byte) (c << 2);
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(j);
+
+        int k = -1;
+        System.out.println((k >> 24));
+        System.out.println((k >>> 24));
     }
 
     
