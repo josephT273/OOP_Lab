a94e0681c9ccfbb77a21bbcfb79ff3d65ef40942
josepht273 <josephtadessem@gmail.com>
2025-03-23 11:32:07 +0300

    Artimatic Opration

---
+++ src/Addition.java
@@ -0,0 +1,27 @@
+// Single line comment
+/*
+ * multi line comment
+ * ans here it goes
+*/
+
+/**
+ * Documentation comment
+ * like this
+ * @params int a
+ * @author Joseph Tadesse
+ * @version 1.0
+ */
+
+
+public class Addition {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        System.out.println("Sum " + x + y);
+        System.out.println("Sub " + (x - y));
+        System.out.println("Multi " + x * y);
+        System.out.println("Div " + x / y);
+    }
+
+    
+}
